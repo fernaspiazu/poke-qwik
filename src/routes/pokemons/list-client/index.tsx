af3735fbefa571ade5a7d091210ed5361d67bda9
Fernando Aspiazu <fe.aspiazu@gmail.com>
2024-01-25 01:32:36 +0100

    Use of useStore()

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,11 +1,51 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useStore } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
+import type { SmallPokemon } from "~/interfaces";
 
-// import moduleStyles from "../../styles.css?inline";
+interface PokemonPageState {
+  currentPage: number;
+  pokemons: SmallPokemon[];
+}
 
 export default component$(() => {
-  // useStylesScoped$(moduleStyles);
-  return <span>Hello World - List Client</span>;
+  const pokemonState = useStore<PokemonPageState>({
+    currentPage: 0,
+    pokemons: [],
+  });
+
+  return (
+    <>
+      <div class="flex flex-col">
+        <span class="my-5 text-5xl">Status</span>
+        <span>Current page: {pokemonState.currentPage}</span>
+        <span>Loading page: </span>
+      </div>
+
+      <div class="mt-10">
+        <button
+          onClick$={() => pokemonState.currentPage--}
+          class="btn btn-primary mr-2"
+        >
+          Previous
+        </button>
+        <button
+          onClick$={() => pokemonState.currentPage++}
+          class="btn btn-primary mr-2"
+        >
+          Next
+        </button>
+      </div>
+
+      <div class="mt-5 grid grid-cols-5">
+        {/* {pokemonResponse.value.map(({ id, name }) => (
+          <div key={name} class="m-5 flex flex-col items-center justify-center">
+            <PokemonImage id={id} isVisible />
+            <span class="capitalize">{name}</span>
+          </div>
+        ))} */}
+      </div>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
