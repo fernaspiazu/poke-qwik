fe0894f809225b8846066fd22c9abac4cb322cff
Fernando Aspiazu <fe.aspiazu@gmail.com>
2024-01-25 02:23:21 +0100

    Run prettier

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,2 @@
-export * from "./pokemon-list.response"
-export * from "./small-pokemon"
+export * from "./pokemon-list.response";
+export * from "./small-pokemon";
--- src/interfaces/small-pokemon.ts
+++ src/interfaces/small-pokemon.ts
@@ -1,4 +1,4 @@
 export interface SmallPokemon {
-  id: string
-  name: string
+  id: string;
+  name: string;
 }
