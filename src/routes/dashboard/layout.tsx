140527d8ccfda9cd8cbe33d5c51100ed1c2f15ba
Fernando Aspiazu <fe.aspiazu@gmail.com>
2024-02-02 18:27:44 +0100

    Secure route

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -1,5 +1,15 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import Navbar from "~/components/shared/navbar/navbar";
+
+export const useCheckAuthCookie = routeLoader$(({ cookie, redirect }) => {
+  const jwtCookie = cookie.get("jwt");
+  if (jwtCookie && jwtCookie.value === "This is my JWT") {
+    return;
+  }
+
+  throw redirect(302, "/login");
+});
 
 export default component$(() => {
   return (
